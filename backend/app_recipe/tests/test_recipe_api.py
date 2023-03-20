e4d6aa341b979c768bac94306e15d3e41fb32634
devZeylon <93649385+devZeylon@users.noreply.github.com>
2023-03-20 18:02:47 +0100

    app_recipe
    Added new API application => app_recipe.

---
+++ backend/app_recipe/tests/test_recipe_api.py
@@ -0,0 +1,22 @@
+"""
+Tests for recipe APIs.
+"""
+from decimal import Decimal
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APIClient
+from app_core.models import Recipe
+from app_recipe.api.serializers import RecipeSerializer
+
+
+def create_recipe(user, **params):
+    """Create and return a sample recipe."""
+    defaults = {
+        "title": "Sample recipe title",
+        "time_minutes": 22,
+        "price": Decimal("5.25"),
+        "description": "Sample description",
+        "link": "http://example.com/recipe.pdf",
+    }
